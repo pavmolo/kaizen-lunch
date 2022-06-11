1fdf3160e1ea24167c5e223c4371d6fe1d722807
pavmolo <74321783+pavmolo@users.noreply.github.com>
2022-06-11 22:56:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from datetime import date
 from notion.client import NotionClient
 import requests, json
 client = NotionClient(token_v2="3b7c4e7acf1f99610e191d44b024dd9ba201e470a96860d2e1607fba7370497a5c0500cb3c49319ae8cc28607d72021bf6a388018475e9011fa9e891c6b3e57c10ee832634524a77068a6a4b2d89")
-page = client.get_block("https://www.notion.so/kaizeninstitute/4b77f19c3cbb4d8ea289eefd438da180")
+page = client.get_block("https://www.notion.so/kaizeninstitute/4b77f19c3cbb4d8ea289eefd438da180?v=973c72e0c791442e937f86ca5a5c2f44")
 
 
 gsheetid = '1ubyAIc1JOWLRXz-vvTmfhbi1-AZALWKkQo8hJkfvVrc'
