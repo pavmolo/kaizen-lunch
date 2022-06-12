ce9cc4bb64d4ca7f9dc502f4943ed453bc4256d2
pavmolo <74321783+pavmolo@users.noreply.github.com>
2022-06-12 16:10:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -44,32 +44,31 @@ def show_predict_page():
     d = st.date_input("Сегодня:", today)
     st.write('Текущая дата:', d)
     member = st.radio('Выберите едока', df_member_list, index=0, horizontal=True)
-    col1, col2 = st.columns(2)
     order_list = []
     for i in range(0, ln_list):
-        dish_list_radio = col1.radio(df_dish_list.index[i], [int(0), int(1), int(2)], index=0, horizontal=True)
+        dish_list_radio = st.radio(df_dish_list.index[i], [int(0), int(1), int(2)], index=0, horizontal=True)
         dish_item = [df_dish_list.index[i], dish_list_radio]
         order_list.append(dish_item)
     order_list = pd.DataFrame(order_list, columns=['Блюдо', 'Количество'])
     order_list_not_zero = order_list[order_list['Количество'] > 0]
-    col2.dataframe(data=order_list_not_zero, width=None, height=None)
+    st.dataframe(data=order_list_not_zero, width=None, height=None)
     order_list_full = order_list_not_zero
     order_list_full['Едок'] = member
     order_list_full['Дата'] = str(today)
     order_list_full['Количество'] = order_list_full['Количество'].astype('str')
-    col2.dataframe(data=order_list_full, width=None, height=None)
-    if col2.button('Отправить запрос на еду'):
+    st.dataframe(data=order_list_full, width=None, height=None)
+    if st.button('Отправить запрос на еду'):
       body = []
       for row in order_list_full.index:
         stroka = [order_list_full.loc[row][3], order_list_full.loc[row][2], order_list_full.loc[row][0], order_list_full.loc[row][1]]
         body.append(stroka)
       resp = service.append(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME, valueInputOption='USER_ENTERED', body={'values': body}).execute()
-      col2.write('Запрос принят')
+      st.write('Запрос принят')
       today_dish_list_csv = "https://docs.google.com/spreadsheets/d/{}/gviz/tq?tqx=out:csv&sheet={}".format(SAMPLE_SPREADSHEET_ID, SAMPLE_RANGE_NAME)
       today_dish_list = pd.read_csv(today_dish_list_csv)
       today_dish_list['Дата'] = pd.to_datetime(today_dish_list['Дата']).dt.date
       today_dish_list_fin = today_dish_list[today_dish_list['Дата'] == pd.Timestamp.today().date()]
-      col2.dataframe(data=today_dish_list_fin, width=None, height=None)
+      st.dataframe(data=today_dish_list_fin, width=None, height=None)
       table = pd.pivot_table(today_dish_list_fin, values='Количество', index='Блюдо', aggfunc=sum)
       st.dataframe(data=table, width=None, height=None)
 # Вызываем приложение
